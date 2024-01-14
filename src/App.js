9a1135d91cdc9507de6a4de2c378e9082676638e
jstewart8053 <jstewart8053@gmail.com>
2024-01-14 11:23:20 -0500

    added close box icon

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const messages = [
 function App() {
 
 const [step, setStep] = useState(1);
+const [isOpen, setIsOpen] = useState(true);
 
 function handlePrevious() {
 step > 1 && ( 
@@ -20,7 +21,14 @@ function handleNext() {
   setStep(step + 1));
 }
 
-  return <div className= "steps">
+  return (
+    <div>
+  <button className="close" onClick={() => setIsOpen(!isOpen)}>
+    &times;
+    </button>
+    
+  {isOpen && (
+  <div className= "steps">
 <div className="numbers">
   <div className={`${step >=1 ? 'active': ""}`}>1</div>
   <div className={`${step >=2 ? 'active': ""}`}>2</div>
@@ -29,19 +37,21 @@ function handleNext() {
 
 <p className="message">Step {step}: {messages[step- 1]}</p>
 <div className="buttons">
-<button style={{backgroundColor: "#7950f2", color: "fff"}}
+<button style={{backgroundColor: "#7950f2", color: "#fff"}}
 onClick={handlePrevious}> Previous
 
 </button>
 
 
-<button style={{backgroundColor: "#7950f2", color: "fff"}}
+<button style={{backgroundColor: "#7950f2", color: "#fff"}}
 onClick={handleNext}
 >Next</button>
 
 </div>
   </div>
-  
-}
+  )}
+    </div>
+  );
+  };
 
 export default App
